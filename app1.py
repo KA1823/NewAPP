2d2e8a0ef364cd0cf0a89a1993820759a003d6df
KA1823 <118958591+KA1823@users.noreply.github.com>
2022-11-28 13:31:42 +0500

    Dev (#2)
    * update app file
    
    * new code
    
    Co-authored-by: Kamran Anwar <kamran.anwat091@gmail.com>

--- app1.py
+++ app1.py
@@ -1,1 +1,15 @@
-Hello world! kami
+from flask import Flask
+# request
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def hello():
+   # print (request.headers)
+   return "Welcome Flask APP"
+
+
+
+# if __name__ == "__main__":
+#    app.run()
